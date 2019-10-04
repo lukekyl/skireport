17cbd6555d04a42fce7b4b8764e413cd8d7b3163
Kyle Luke <th.oldschool@gmail.com>
2019-10-04 00:15:10 +0000

    updates 10/3

--- lib/skireport/cli.rb
+++ lib/skireport/cli.rb
@@ -29,39 +29,12 @@ def menu
   input = nil
   while input != "exit"
     input = gets.strip.downcase
-    case input
-      when "1"
-        puts "Aspen weather is ..."
-          #https://www.wunderground.com/weather/us/co/aspen/81611
-      when "2"
-        puts "Beaver Creek weather is ..."
-          #https://www.wunderground.com/weather/us/co/avon/KCOAVON21
-      when "3"
-        puts "Breckenridge weather is ..."
-          #https://www.wunderground.com/weather/us/co/breckenridge/KCOBRECK63
-      when "4"
-        puts "Copper Mountain weather is ..."
-          #https://www.wunderground.com/weather/us/co/copper-mountain/80443
-      when "5"
-        puts "Crested Butte weather is ..."
-          #https://www.wunderground.com/weather/us/co/crested-butte/81224
-      when "6"
-        puts "Keystone weather is ..."
-          #https://www.wunderground.com/weather/us/co/dillon/KCODILLO40
-      when "7"
-        puts "Steamboat Springs weather is ..."
-          #https://www.wunderground.com/weather/us/co/steamboat-springs/80487
-      when "8"
-        puts "Telluride weather is ..."
-          #https://www.wunderground.com/weather/us/co/telluride/81435
-      when "9"
-        puts "Vail weather is ..."
-          #https://www.wunderground.com/weather/us/co/vail/81657
-      when "10"
-        puts "Winter Park weather is ..."
-          #https://www.wunderground.com/weather/us/co/winter-park/80482
-      when "list"
+    if input == "list"
         list_resorts
+    elsif input.to_i >= 0 && input.to_i <= 10
+        site_link(input)
+    else
+      puts "Please type a valid resort number, list, or exit."
     end
   end
 end
@@ -73,32 +46,55 @@ end
 
 def site_link(input)
   case input
-    when "1" #Aspen
+    when "1"
+      name = "Aspen"
       link =  "https://www.wunderground.com/weather/us/co/aspen/81611"
-    when "2" #Beaver Creek
+      puts "#{name} link is #{link}"
+    when "2"
+      name = "Beaver Creek"
       link = "https://www.wunderground.com/weather/us/co/avon/KCOAVON21"
-    when "3" #Breckenridge
+      puts "#{name} link is #{link}"
+    when "3"
+      name = "Breckenridge"
       link = "https://www.wunderground.com/weather/us/co/breckenridge/KCOBRECK63"
-    when "4" #Copper Mountain
+      puts "#{name} link is #{link}"
+    when "4"
+      name = "Copper Mountain"
       link = "https://www.wunderground.com/weather/us/co/copper-mountain/80443"
-    when "5" #Crested Butte
+      puts "#{name} link is #{link}"
+    when "5"
+      name = "Crested Butte"
       link = "https://www.wunderground.com/weather/us/co/crested-butte/81224"
-    when "6" #Keystone
+      puts "#{name} link is #{link}"
+    when "6"
+      name = "Keystone"
       link = "https://www.wunderground.com/weather/us/co/dillon/KCODILLO40"
-    when "7" #Steamboat Springs
+      puts "#{name} link is #{link}"
+    when "7"
+      name = "Steamboat Springs"
       link = "https://www.wunderground.com/weather/us/co/steamboat-springs/80487"
-    when "8" #Telluride
+      puts "#{name} link is #{link}"
+    when "8"
+      name = "Telluride"
       link = "https://www.wunderground.com/weather/us/co/telluride/81435"
-    when "9" #Vail
+      puts "#{name} link is #{link}"
+    when "9"
+      name = "Vail"
       link = "https://www.wunderground.com/weather/us/co/vail/81657"
-    when "10" #Winter Park
+      puts "#{name} link is #{link}"
+    when "10"
+      name = "Winter Park"
       link = "https://www.wunderground.com/weather/us/co/winter-park/80482"
+      puts "#{name} link is #{link}"
   end
+  get_resort(name,link)
   link
 end
 
-def scrape_conditions(link)
-  
+def get_resort(name, link)
+  resort = SkiReport::Resort.new(name)
+  resort.scrape_conditions(link)
+  p resort
 end
 
 end
--- lib/skireport/resort.rb
+++ lib/skireport/resort.rb
@@ -1,5 +1,6 @@
 class SkiReport::Resort
-
+  attr_accessor :name, :current, :high, :low, :precip, :snow
+  @@all = []
   # Ski Conditions at:
   # Beaver Creek Ski Resort
   # Current Weather is 49 f and Sunny
@@ -8,5 +9,24 @@ class SkiReport::Resort
   # Chance of Precip. - 0%
   # Snow Depth - 1 in
   # Tomorrows High - 50 f
+  def initialize(name)
+    @name = name
+    save
+  end
+  def save
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+
+  def scrape_conditions(link)
+    doc = Nokogiri::HTML(open(link))
+    self.current = doc.search("div.current-temp span.wu-value").text
+    self.high = doc.search("span.hi").text
+    self.low = doc.search("span.lo").text
+    self.precip = doc.search("div#precipBarChart text").text
+    self.snow = doc.search("span.wu-unit-snow span.wu-value-to").text
+  end
 
 end
